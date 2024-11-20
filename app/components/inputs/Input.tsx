0adf4bc49e6744f1b4634eb4bbfc010603bc0894
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-11-20 20:38:49 +0200

    Register Func, setup MongoDB, Prisma && fix Input UI

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
 import { BiDollar } from 'react-icons/bi';
 interface InputProps {
@@ -13,6 +13,11 @@ interface InputProps {
         errors: FieldErrors;
 }
 const Input: React.FC<InputProps> = ({ id, label, type = 'text', disabled, formatPrice, required, register, errors }) => {
+        const [value, setValue] = useState('');
+
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+                setValue(e.target.value);
+        };
         return (
                 <div className="w-full relative">
                         {formatPrice && <BiDollar size={24} className="text-neutral-700 absolute top-5 left-2" />}
@@ -21,7 +26,9 @@ const Input: React.FC<InputProps> = ({ id, label, type = 'text', disabled, forma
                                 disabled={disabled}
                                 {...register(id, { required })}
                                 placeholder=" "
-                                type="type"
+                                value={value}
+                                onChange={handleChange}
+                                type={type}
                                 className={`peer w-full p-4 pt-6  font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed 
                                         ${formatPrice ? 'pl-9' : 'pl-4'} 
                                         ${errors[id] ? 'border-rose-500' : 'border-neutral-300'} 
@@ -29,12 +36,10 @@ const Input: React.FC<InputProps> = ({ id, label, type = 'text', disabled, forma
                         />
                         <label
                                 className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] 
-                                        ${formatPrice ? 'left-9' : 'left-4'} 
-                                        peer-placeholder-shown:scale-100 
-                                        peer-placeholder-shown:translate-y-0 
-                                        peer-focus:scale-75 
-                                        peer-focus:-translate-y-4
-                                        ${errors[id] ? 'text-rose-500' : 'text-neutral-700'}`}
+                                ${formatPrice ? 'left-9' : 'left-4'} 
+                                ${value ? 'scale-75 -translate-y-4' : 'peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0'} 
+                                ${errors[id] ? 'text-rose-500' : 'text-neutral-700'} 
+                                peer-focus:scale-75 peer-focus:-translate-y-4`}
                         >
                                 {label}
                         </label>
